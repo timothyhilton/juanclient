0d537599a951467a571e8e9877102f1b45a1200e
timothyhilton <recore444@gmail.com>
2024-07-07 22:58:02 +1000

    added temporary rainbow theme to arraylist and other small tweaks

--- src/minecraft/juan/modules/render/OldSwing.java
+++ src/minecraft/juan/modules/render/OldSwing.java
@@ -11,7 +11,7 @@ import net.minecraft.client.renderer.GlStateManager;
 
 public class OldSwing extends Module {
 	
-	ModeSetting swingType = new ModeSetting("Type", "1.7ish", "1.7ish", "test");
+	ModeSetting swingType = new ModeSetting("Type", "1.7ish", "1.7ish", "test", "high");
 	
 	public OldSwing() {
 		super("OldSwing", Keyboard.KEY_NONE, Category.RENDER, true);
@@ -39,7 +39,17 @@ public class OldSwing extends Module {
 				case "test":
 					event.ir.transformFirstPersonItem(event.f, event.swingProgress);
 		            
-					GlStateManager.translate(-0.5F, 0.4F, 0.0F);
+					GlStateManager.translate(-0.5F, 0.F, 0.0F);
+			        GlStateManager.rotate(30.0F, 0.0F, 1.0F, 0.0F);
+			        GlStateManager.rotate(-80.0F, 1.0F, 0.0F, 0.0F);
+			        GlStateManager.rotate(60.0F, 0.0F, 1.0F, 0.0F);
+		            GlStateManager.rotate(-18.0F, 0.0F, 0.0F, 1.0F);
+		            
+		            break;
+				case "high":
+					event.ir.transformFirstPersonItem(event.f, event.swingProgress);
+		            
+					GlStateManager.translate(-0.5F, 0.8F, 0.0F);
 			        GlStateManager.rotate(30.0F, 0.0F, 1.0F, 0.0F);
 			        GlStateManager.rotate(-80.0F, 1.0F, 0.0F, 0.0F);
 			        GlStateManager.rotate(60.0F, 0.0F, 1.0F, 0.0F);
