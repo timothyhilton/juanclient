0f13e4dcaa0389416fd7f094428af1148940a5d1
timothyhilton <recore444@gmail.com>
2024-07-08 00:42:10 +1000

    added more themes

--- src/minecraft/juan/ui/HUD.java
+++ src/minecraft/juan/ui/HUD.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 import juan.Client;
 import juan.modules.Module;
 import juan.modules.render.Theme;
+import juan.settings.BooleanSetting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
@@ -19,12 +20,13 @@ public class HUD {
 	
 	public void draw() {
 		FontRenderer fr = mc.fontRendererObj;
+		Boolean useTheme = ((BooleanSetting) Client.theme.settings.get(1)).enabled;
 		
 		GlStateManager.pushMatrix();
 		GlStateManager.translate(4, 4, 0);
 		GlStateManager.scale(1.5, 1.5, 1);
 		GlStateManager.translate(-4, -4, 0);
-		fr.drawStringWithShadow(Client.name + " " + Client.version, 4, 4, ((Theme) Client.getModuleByName("Theme")).getColour(0));
+		fr.drawStringWithShadow(Client.name + " " + Client.version, 4, 4, (useTheme ? Client.theme.getColour(0) : -1));
 		GlStateManager.popMatrix();
 		
 		Client.onEvent(new EventRenderGUI());
